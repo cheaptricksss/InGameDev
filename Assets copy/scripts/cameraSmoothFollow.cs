41ec846c29646fd1fe8b58a3ce2617fc57a89d72
cheaptricksss <dao9828@nyu.edu>
2022-09-25 16:21:15 -0400

    major changes, almost done with the mechanism

---
+++ Assets copy/scripts/cameraSmoothFollow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraSmoothFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float followSpeed = 0.125f;
+    public Vector3 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
